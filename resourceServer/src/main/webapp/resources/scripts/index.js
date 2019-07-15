0d18537cb676944f4fd7950173eb2a0b8a4efa11
sanzhar-at-402975993256 <Eqpg/8ab9RZOTGHs7Srf0SpHlw7EhBhRxUIuhNMp8kw=>
2019-07-15 09:59:40 +0600

    fix

--- resourceServer/src/main/webapp/resources/scripts/index.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 10) {
-        $(".bar").removeClass("scroll");
-    } else {
-        $(".bar").addClass("scroll");
-    }
-});
